9c9860cf590483abd939c23a70d959213658f120
Jethro Cao <jethrocao@gmail.com>
2019-12-06 21:01:57 +0700

    Switch source method: git-fetch -> url-fetch

--- ripgrep.scm
+++ ripgrep.scm
@@ -1,23 +1,21 @@
 (define-module (ripgrep)
                #:use-module (guix packages)
-               #:use-module (guix git-download)
+               #:use-module (guix download)
                #:use-module (guix build-system cargo)
                #:use-module (guix licenses))
 
 (define-public ripgrep
- (let ((commit "8892bf648cfec111e6e7ddd9f30e932b0371db68")
-       (srchash "03nxmr0cyhj8aq1d8jk6yy94z447s4hzyg7c5zgxjxpmapj9c49k")
-       (revision "+54days"))
   (package
    (name "ripgrep")
-   (version (git-version "11.0.2" revision commit))
+   (version "11.0.2")
    (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://github.com/BurntSushi/ripgrep")
-                  (commit commit)))
-            (file-name (git-file-name name version))
-            (sha256 (base32 srchash))))
+            (method url-fetch)
+            (uri (string-append
+                  "https://github.com/BurntSushi/ripgrep/archive/"
+                  version
+                  ".tar.gz"))
+            (sha256 (base32 
+                     "0m8094rspl6csv17ak548kmk9bchsmixdmd6qy5xlslkg498d0q9"))))
    (build-system cargo-build-system)
    (arguments
     `(#:cargo-build-flags '("--release")
@@ -28,7 +26,7 @@ searches your current directory for a regex pattern. By default,
 ripgrep will respect your .gitignore and automatically skip hidden
 files/directories and binary files.")
    (home-page "https://github.com/BurntSushi/ripgrep")
-   (license (list license:expat license:unlicense)))))
+   (license (list license:expat license:unlicense))))
 
 ;; failed build: Dec. 06 2019
 ;; note: keeping build directory `/tmp/guix-build-ripgrep-11.0.2-+54days.8892bf6.drv-0'
